517503aabf3ab368b844e98d68570115d1a93070
Masaki Takeuchi <m.ishihara@gmail.com>
2016-12-26 02:59:47 +0000

    Add a `processing` metric for a scale down alarm

--- lib/resque/cloudwatch/metrics/metric.rb
+++ lib/resque/cloudwatch/metrics/metric.rb
@@ -17,7 +17,10 @@ module Resque
                 namespace,
                 Resque.info,
                 current_queue_sizes,
-              )
+                get_previous_processed(namespace),
+              ).tap do |metric|
+                set_previous_processed(namespace, metric.processed)
+              end
             end
           end
 
@@ -26,20 +29,42 @@ module Resque
           def current_queue_sizes
             Resque.queues.map { |name| [name, Resque.size(name)] }
           end
+
+          def get_previous_processed(namespace)
+            @_previous_processed ||= {}
+            @_previous_processed[namespace]
+          end
+
+          def set_previous_processed(namespace, previous_processed)
+            @_previous_processed ||= {}
+            @_previous_processed[namespace] = previous_processed
+          end
         end
 
-        def initialize(time, namespace, info, queue_sizes)
+        def initialize(time, namespace, info, queue_sizes, previous_processed)
           @time = time
           @namespace = namespace
           @info = info
           @queue_sizes = queue_sizes
+          @previous_processed = previous_processed
+        end
+
+        def pending;   @info[:pending];   end
+        def processed; @info[:processed]; end
+        def failed;    @info[:failed];    end
+        def queues;    @info[:queues];    end
+        def workers;   @info[:workers];   end
+        def working;   @info[:working];   end
+
+        def processing
+          incremental_size_of_processed + working
         end
 
         def to_cloudwatch_metric_data
           dimensions = [{ name: 'namespace', value: @namespace.to_s }]
 
-          %i(pending processed failed queues workers working).map do |key|
-            build_cloudwatch_metric_datum(key.to_s.capitalize, @info[key])
+          %i(pending processed failed queues workers working processing).map do |key|
+            build_cloudwatch_metric_datum(key.to_s.capitalize, public_send(key))
           end +
           @queue_sizes.map do |name, size|
             build_cloudwatch_metric_datum('Pending', size, queue: name)
@@ -60,6 +85,14 @@ module Resque
 
         def default_dimensions
           @_default_dimensions = Dimensions.new(namespace: @namespace.to_s)
+        end
+
+        def incremental_size_of_processed
+          if @previous_processed.nil?
+            0
+          else
+            [0, processed - @previous_processed].max
+          end
         end
       end
 
